9cf7aca55d2f8f2b9c1505313dc7d209d6dcd71b
pengli <365627310@qq.com>
2021-12-08 17:13:43 +0800

    Initial commit

---
+++ leyuna-disk-start/src/main/java/com/leyuna/disk/start/DiskStartApplication.java
@@ -0,0 +1,13 @@
+package com.leyuna.disk.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskStartApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(DiskStartApplication.class, args);
+    }
+
+}
