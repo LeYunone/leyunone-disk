33de5363fa1be531bedef989e53f27f927cb5379
pengli <365627310@qq.com>
2021-12-09 14:59:32 +0800

    [add]

---
+++ leyuna-disk-client/src/main/java/com/leyuna/disk/client/dto/DataResponse.java
@@ -0,0 +1,12 @@
+package com.leyuna.disk.client.dto;
+
+/**
+ * @author pengli
+ * @create 2021-12-09 11:02
+ */
+public class DataResponse<T> {
+
+    private T data;
+
+
+}
---
+++ leyuna-disk-web/src/main/java/com/leyuna/disk/web/controll/FileController.java
@@ -0,0 +1,16 @@
+package com.leyuna.disk.web.controll;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pengli
+ * @create 2021-12-09 10:38
+ * 文件方接口
+ */
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+
+}
