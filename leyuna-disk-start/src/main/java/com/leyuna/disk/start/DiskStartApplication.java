ecdfbc88310f966d47332bc26d8d67768ce630e9
pengli <365627310@qq.com>
2022-02-16 10:30:26 +0800

    [UPDATE]修改启动方式 外置tomcat

--- leyuna-disk-start/src/main/java/com/leyuna/disk/start/DiskStartApplication.java
+++ leyuna-disk-start/src/main/java/com/leyuna/disk/start/DiskStartApplication.java
@@ -2,6 +2,8 @@ package com.leyuna.disk.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -12,10 +14,15 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableCaching //开启缓存
 @RefreshScope
 @EnableDiscoveryClient
-public class DiskStartApplication {
+public class DiskStartApplication extends SpringBootServletInitializer {
 
     public static void main (String[] args) {
         SpringApplication.run(DiskStartApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DiskStartApplication.class);
     }
 
 }
