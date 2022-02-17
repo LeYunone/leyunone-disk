c48c592404e2fa6edc70849ec4bb72eef38a49be
pengli <365627310@qq.com>
2022-02-17 15:52:47 +0800

    [UPDATE]微服务项目提供jar包， 注意以后别在jar包项目中设置项目路径

---
+++ leyuna-disk-dao/src/main/java/com/leyuna/disk/config/NacosListener.java
@@ -0,0 +1,35 @@
+package com.leyuna.disk.config;
+
+import com.alibaba.cloud.nacos.registry.NacosAutoServiceRegistration;
+import com.alibaba.cloud.nacos.registry.NacosRegistration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author pengli
+ * @create 2022-02-17 10:23
+ */
+@Component
+public class NacosListener implements ApplicationListener<ApplicationReadyEvent> {
+    @Autowired
+    private NacosRegistration registration;
+
+    @Autowired
+    private NacosAutoServiceRegistration nacosAutoServiceRegistration;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        String property = event.getApplicationContext().getEnvironment().getProperty("server.port");
+        registration.setPort(Integer.valueOf(property));
+        nacosAutoServiceRegistration.start();
+    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void onWebServerReady(ApplicationReadyEvent event) {
+        String property = event.getApplicationContext().getEnvironment().getProperty("server.port");
+        registration.setPort(Integer.valueOf(property));
+        nacosAutoServiceRegistration.start();
+    }
+}
--- leyuna-disk-start/src/main/java/com/leyuna/disk/start/DiskStartApplication.java
+++ leyuna-disk-start/src/main/java/com/leyuna/disk/start/DiskStartApplication.java
@@ -6,13 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication()
+@SpringBootApplication
 @ComponentScan({"com.leyuna.disk"})
 @EnableCaching //开启缓存
-@RefreshScope
 @EnableDiscoveryClient
 public class DiskStartApplication extends SpringBootServletInitializer {
 
